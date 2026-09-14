70e1e3af20495c90854102e27eab9e9fd255df39
JiongXiaXia <jiongxiagu@outlook.com>
2026-09-14 17:19:30 +0800

    Load main menu refinement styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './workspace.css';
 import './tool-overlay.css';
+import './menu-refine.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
